af5f75960737756405c304b04ed28481331ab7cf
quat1024 <quat1024@users.noreply.github.com>
2022-06-03 20:57:42 -0400

    more duct tape

--- src/main/java/agency/highlysuspect/funnyducttape/mixin/client/ClientboundPlayerInfoPacketMixin.java
+++ src/main/java/agency/highlysuspect/funnyducttape/mixin/client/ClientboundPlayerInfoPacketMixin.java
@@ -9,9 +9,41 @@ import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ClientboundPlayerInfoPacket.class)
 public class ClientboundPlayerInfoPacketMixin {
+	static {
+		for(int i = 0; i < 20; i++) {
+			FunnyDuctTape.LOGGER.info("Conspicuous Log Spam that shows ClientBoundPlayerInfoPacketMixin succeeded !!!!!!!!!!!!!!!!!!! Ok!!!!!!!!!!");
+		}
+	}
+	
+	@Redirect(
+		method = "<init>(Lnet/minecraft/network/FriendlyByteBuf;)V",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/FriendlyByteBuf;readEnum(Ljava/lang/Class;)Ljava/lang/Enum;"
+		)
+	)
+	private <T extends Enum<T>> T asdahjdjdkj(FriendlyByteBuf friendlyByteBuf, Class<T> class_) {
+		int oldIndex = friendlyByteBuf.readerIndex();
+		
+		int len = friendlyByteBuf.readableBytes();
+		FunnyDuctTape.LOGGER.info("Got ClientboundPlayerInfoPacket length {}. Attempting to hex dump idk im not good at bytebufs", len);
+		StringBuilder hex = new StringBuilder();
+		for(int i = 0; i < len; i++) {
+			hex.append(String.format("%02x", friendlyByteBuf.readByte()));
+		}
+		FunnyDuctTape.LOGGER.info("Hexdump: {}", hex);
+		
+		friendlyByteBuf.readerIndex(oldIndex);
+		
+		//original method
+		return friendlyByteBuf.readEnum(class_);
+	}
+	
 	/**
 	 * @author quat
 	 * @reason Your mom
@@ -36,7 +68,11 @@ public class ClientboundPlayerInfoPacketMixin {
 			FunnyDuctTape.LOGGER.info("Successfully parsed");
 			return component;
 		} catch (Exception e) {
-			e.printStackTrace();
+			FunnyDuctTape.LOGGER.error("CRASH!!!!!!!", e);
+			
+			FunnyDuctTape.LOGGER.error("Trying to salvage FriendlyByteBuf");
+			buf.readerIndex(buf.readableBytes() - 1);
+			
 			return new TextComponent("[FunnyDuctTape] Crash in ClientboundPlayerInfoPacket#readComponent. See log").withStyle(ChatFormatting.RED);
 		}
 	}
---
+++ src/main/java/agency/highlysuspect/funnyducttape/mixin/client/PacketDecoderMixin.java
@@ -0,0 +1,28 @@
+package agency.highlysuspect.funnyducttape.mixin.client;
+
+import agency.highlysuspect.funnyducttape.FunnyDuctTape;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.PacketDecoder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(PacketDecoder.class)
+public class PacketDecoderMixin {
+	@Redirect(
+		method = "decode",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/FriendlyByteBuf;readableBytes()I",
+			ordinal = 1
+		)
+	)
+	private int whenYou(FriendlyByteBuf instance) {
+		int realReadableBytes = instance.readableBytes();
+		if(realReadableBytes > 0) {
+			FunnyDuctTape.LOGGER.info("ReadableBytes > 0 ({}) after reading a packet in PacketDecoder!!!!!!! this is a sign of a HUGE PROBLEM!!!!!!!! Trying to overlook it in order to salvage what's left of this game connection", realReadableBytes);
+		}
+		
+		return 0;
+	}
+}
